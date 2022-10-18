28d1b0f8ac162b47ad657ccabae93d6742d1deed
alcl2000 <amy.555.lewis@gmail.com>
2022-10-18 17:31:59 +0000

    python: get_sales_data started

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+
+def get_sales_data():
+    """
+    Get sales figures from input by user
+    """
+    print('Please enter sales data from the last market')
+    print('The data should be six figures, seperated by commas')
+    print('Example: 3,4,1,66,12,4\n')
+
+    data_str = input('Enter your data here: ')
+    sales_data = data_str.split(',')
+    print(f'The value of your data is {sales_data}')
+
+
+get_sales_data()
