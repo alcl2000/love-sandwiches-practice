71f2cab29d6ffe63e5b53e0e9e3151732e9be055
alcl2000 <amy.555.lewis@gmail.com>
2022-10-19 09:40:29 +0000

    add data request loop until valid data provided

--- run.py
+++ run.py
@@ -17,13 +17,17 @@ def get_sales_data():
     """
     Get sales figures from input by user
     """
-    print('Please enter sales data from the last market')
-    print('The data should be six figures, seperated by commas')
-    print('Example: 3,4,1,66,12,4\n')
+    while True:
+        print('Please enter sales data from the last market')
+        print('The data should be six figures, seperated by commas')
+        print('Example: 3,4,1,66,12,4\n')
 
-    data_str = input('Enter your data here: ')
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+        data_str = input('Enter your data here: ')
+        sales_data = data_str.split(',')
+
+        if validate_data(sales_data):
+            print('Data is valid')
+            break
 
 
 def validate_data(values):
@@ -32,12 +36,15 @@ def validate_data(values):
     if so it converts strings into intergers
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly 6 values are required, you entered {len(values)} "
+                f"Exactly 6 values are required, you entered {len(values)}"
             )
     except ValueError as e:
         print(f'Invalid data: {e}, please try again')
+        return False
+    return True
 
 
 get_sales_data()
